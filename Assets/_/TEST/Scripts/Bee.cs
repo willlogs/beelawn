a4d5b32f9e639b5f1c79ff2dbf29634af571c8c0
Willie <m4ds31@gmail.com>
2021-08-19 17:05:51 +0430

    bee movement

---
+++ Assets/_/TEST/Scripts/Bee.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace PT.Bees
+{
+    public class Bee : MonoBehaviour
+    {
+        public Vector3 position;
+
+        public void SetParent(Bee p, BeeChain c)
+        {
+            _parent = p;
+            _hasParent = true;
+
+            _chain = c;
+            _hasChain = true;
+        }
+
+        public void SetChain(BeeChain c)
+        {
+            _chain = c;
+            _hasChain = true;
+        }
+
+        public void Follow(bool f)
+        {
+            _outtafollow = f;
+            if (f)
+            {
+                _follow = _outtafollow;
+                OnFollowChnaged();
+            }
+        }
+
+        [SerializeField] private float _delay = 0.2f, _tolerance = 0.2f;
+        [SerializeField] private Transform _beeBodyT;
+        private Bee _parent;
+        private BeeChain _chain;
+        private bool _hasParent, _hasChain, _follow, _outtafollow;
+        private Tweener _randomTweener;
+
+        private void OnFollowChnaged()
+        {
+            if (!_follow)
+            {
+                DoRandomMovement();
+            }
+            else
+            {
+                KillRandomMovement();
+            }
+        }
+
+        private void Update()
+        {
+            if (_follow)
+            {
+                Vector3 random = new Vector3(
+                    Random.Range(-1f, 1f),
+                    Random.Range(-1f, 1f),
+                    Random.Range(-1f, 1f)
+                ).normalized;
+                _beeBodyT.position = Vector3.Lerp(
+                    _beeBodyT.position,
+                    _beeBodyT.position + random,
+                    Time.deltaTime
+                );
+
+                if (_hasParent)
+                {
+                    transform.position = Vector3.Lerp(
+                        transform.position,
+                        _parent.position,
+                        Time.deltaTime / _delay
+                    );
+                }
+                position = transform.position;
+
+                if (
+                    !_outtafollow &&
+                    (
+                        !_hasParent ||
+                        (transform.position - _parent.position).magnitude <= _tolerance
+                    )
+                )
+                {
+                    _follow = _outtafollow;
+                    OnFollowChnaged();
+                }
+            }
+        }
+
+        private void DoRandomMovement()
+        {
+            _beeBodyT.transform.DOLocalMove(Vector3.zero, 0.1f);
+
+            if (!_follow && _hasChain)
+            {
+                Vector3 newPos = new Vector3(
+                    Random.Range(-_chain.radius, _chain.radius),
+                    Random.Range(-_chain.radius, _chain.radius),
+                    Random.Range(-_chain.radius, _chain.radius)
+                ) + _chain.center;
+
+                transform.DOMove(newPos, Random.Range(0.1f, 0.8f)).OnComplete(DoRandomMovement);
+            }
+        }
+
+        private void KillRandomMovement()
+        {
+            _randomTweener.Kill();
+        }
+    }
+}
---
+++ Assets/_/TEST/Scripts/BeeChain.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PT.Bees
+{
+    public class BeeChain : MonoBehaviour
+    {
+        public bool isActive = false;
+        public Vector3 center;
+        public Vector3 goal;
+        public float radius = 2, speed = 5f;
+
+        [SerializeField] private Bee[] _bees;
+        private bool _isActive = true;
+        private Vector3 _lastGoal = Vector3.zero, _lastDiff;
+        private bool _hasLastGoal;
+
+        private void Start()
+        {
+            if (_bees.Length > 0)
+            {
+                _bees[0].SetChain(this);
+            }
+
+            for (int i = 1; i < _bees.Length; i++)
+            {
+                _bees[i].SetParent(_bees[i - 1], this);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                goal = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                if (_hasLastGoal)
+                {
+                    Vector3 diff = goal - _lastGoal;
+                    diff = diff.normalized;
+
+                    if (diff.magnitude == 0)
+                    {
+                        diff = _lastDiff;
+                    }
+
+                    isActive = true;
+
+                    if (_bees.Length > 0)
+                    {
+                        _bees[0].transform.position = Vector3.Lerp(
+                            _bees[0].transform.position,
+                            diff * speed + _bees[0].transform.position,
+                            Time.deltaTime * 5f
+                        );
+                    }
+
+                    _lastDiff = diff;
+                }
+                else
+                {
+                    _hasLastGoal = true;
+                }
+                _lastGoal = goal;
+            }
+            else
+            {
+                isActive = false;
+                _hasLastGoal = false;
+            }
+
+            if (isActive != _isActive)
+            {
+                _isActive = isActive;
+
+                if (!_isActive && _bees.Length > 0)
+                {
+                    center = _bees[0].transform.position;
+                }
+
+                Follow(_isActive);
+            }
+        }
+
+        private void Follow(bool f)
+        {
+            foreach (Bee b in _bees)
+            {
+                b.Follow(f);
+            }
+        }
+    }
+}
